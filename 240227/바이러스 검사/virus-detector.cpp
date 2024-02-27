ee4d77dfb7b959d257d3ffccba984d6811e6de00
Jihyeon Park <142780681+jihyeonly@users.noreply.github.com>
2024-02-27 22:34:52 +0900

    Title: 바이러스 검사, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int arr[1000000];
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    int master, worker;
+    cin >> master >> worker;
+    int check = 0;
+    for (int i = 0; i < n; i++)
+    {
+        check++;
+        arr[i] = arr[i] - master;
+        if (arr[i] > 0)
+        {
+            check += arr[i] / worker;
+            if (arr[i] > 0) check++;
+        }
+    }
+
+    cout << check;
+
+
+    return 0;
+}
