c83a8234b4ec72d0292d16262eec8331ea20aa67
Jihyeon Park <142780681+jihyeonly@users.noreply.github.com>
2024-03-04 22:44:00 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240304/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int arr[100][100];
+int main() {
+    int N, T;
+    string str;
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+    int d = 3;
+    cin >> N >> T;
+    cin >> str;
+    int len = str.length();
+    int r = N / 2;
+    int c = r;
+    int cnt = 1;
+    int x, y;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            arr[i][j] = cnt++;
+        }
+    }
+    int sum = arr[r][c];
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == 'F')
+        {
+            x = r + dx[d];
+            y = c + dy[d];
+            if (0 <= x && x < N && 0 <= y && y < N)
+            {
+                r = x;
+                c = y;
+                //cout << arr[r][c] << endl;
+                sum += arr[r][c];
+            }
+        } else if (str[i] == 'R')
+        {
+            d = (d + 1) % 4;
+        } else if (str[i] == 'L')
+        {
+            d = (d + 3) % 4;
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
