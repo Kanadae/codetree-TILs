a1b7841f9fb8eb8cc237eda148d0b49304ba7337
Jihyeon Park <142780681+jihyeonly@users.noreply.github.com>
2024-03-06 22:37:01 +0900

    Title: 숫자의 순차적 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240306/숫자의 순차적 이동/sequential-movement-of-numbers.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int arr[21][21];
+int main() {
+    int dx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+    int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+    int n, m;
+    int turn = 0;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int x, y;
+    int tx, ty;
+    int idx,idy;
+    int temp;
+    int max;
+    int num = 1;
+    int sig = 0;
+    while (turn < m)
+    {
+        turn++;
+        while (num <= n*n)
+        {
+            sig = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (sig == 1) break;
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i][j] == num)
+                    {
+                        idx = i;
+                        idy = j;
+                        sig = 1;
+                        break;
+                    }
+                }
+            }
+
+        max = INT_MIN;
+        for (int k = 0; k < 8; k++)
+        {
+            x = idx + dx[k];
+            y = idy + dy[k];
+            if (0 <= x && x < n && 0 <= y && y < n)
+            {
+                if (arr[x][y] > max)
+                {
+                    max = arr[x][y];
+                    tx = x;
+                    ty = y;
+
+                }
+            }
+        }
+        temp = arr[idx][idy];
+        arr[idx][idy] = max;
+        arr[tx][ty] = temp;
+        num++;
+        }
+    }
+
+       for (int i = 0; i < n; i++)
+   {
+       for (int j = 0; j < n; j++)
+       {
+           cout << arr[i][j] << " ";
+       }
+       cout << "\n";
+   }
+
+
+    return 0;
+}
