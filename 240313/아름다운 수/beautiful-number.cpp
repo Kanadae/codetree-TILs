47cf4a317524f31370fa09c7179f4915cd586954
Jihyeon Park <142780681+jihyeonly@users.noreply.github.com>
2024-03-13 20:21:06 +0900

    Title: 아름다운 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240313/아름다운 수/beautiful-number.cpp
+++ 240313/아름다운 수/beautiful-number.cpp
@@ -4,48 +4,43 @@
 using namespace std;
 
 int n;
-int cnt = 0;
+int ans;
 vector<int> seq;
 
-bool is_beautiful()
+bool is_beatuiful()
 {
     for (int i = 0; i < n; i += seq[i])
     {
-        if (i + seq[i] - 1 >= n)
-            return false;
+        if (i + seq[i] - 1 >= n) return false;
 
-        for (int j = i; j < i + seq[i]; j++)
+        for (int j = i; j < n; j++)
         {
-            if (seq[j]!=seq[i])
-                return false;
+            if (seq[j] != seq[i]) return false;
         }
     }
 
     return true;
 }
 
-void choose(int cur_num)
+void choose(int cnt)
 {
-    if (cur_num == n)
+    if (cnt == n)
     {
-        if (is_beautiful()) cnt++;
+        if (is_beatuiful()) ans++;
         return;
     }
 
     for (int i = 1; i <= 4; i++)
     {
         seq.push_back(i);
-        choose(cur_num + 1);
+        choose(cnt+1);
         seq.pop_back();
     }
-    
-
-
 }
 
 int main() {
     cin >> n;
     choose(0);
-    cout << cnt;
+    cout << ans;
     return 0;
 }
