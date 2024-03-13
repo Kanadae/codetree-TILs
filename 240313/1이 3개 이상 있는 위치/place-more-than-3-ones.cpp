ad889de41f47bd3c921d53d0101e6e38effe5454
Jihyeon Park <142780681+jihyeonly@users.noreply.github.com>
2024-03-13 21:05:21 +0900

    Title: 1이 3개 이상 있는 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240313/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#define MAX_N 100
+#define DIR_NUM 4
+
+int N;
+int grid[MAX_N][MAX_N];
+using namespace std;
+
+int ans;
+
+void Simulate(int x, int y){
+    int one_num = 0;
+    int dx[DIR_NUM] = {-1, 0, 1, 0}, dy[DIR_NUM] = {0, 1, 0, -1};
+    for(int i=0; i<DIR_NUM; i++){
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+        if(grid[new_x][new_y]) one_num++;
+    }
+    if(one_num >= 3) ans++;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> N;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cin >> grid[i][j];
+        }
+    }
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            Simulate(i, j);
+        }
+    }
+    cout << ans;
+    return 0;
+}
