bd82e3884d836c1ced1e3c28ae9c6cc06d67435c
Jihyeon Park <142780681+jihyeonly@users.noreply.github.com>
2024-03-13 20:57:59 +0900

    Title: 문자에 따른 명령 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240313/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#define DIR_NUM 4
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int dir = 0, x = 0, y = 0;
+    string command;
+    cin >> command;
+    int N = command.length();
+    for(int i=0; i<N; i++){
+        int dx[DIR_NUM] = {-1, 0, 1, 0}, dy[DIR_NUM] = {0, 1, 0, -1};
+        if(command[i] == 'L'){
+            dir = (dir + 3) % 4;
+        }
+        else if(command[i] == 'R'){
+            dir = (dir + 1) % 4;
+        }
+        else{
+            x += dy[dir], y += dx[dir];
+        }
+    }
+    cout << x << " " << y;
+    return 0;
+}
