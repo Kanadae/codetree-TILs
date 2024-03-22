d4aff976968288272da9836b9cb8a796b04e13b1
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-23 00:48:42 +0900

    Title: 겹치지 않게 선분 고르기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240323/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n;
+pair<int, int> line[16];
+int best_cnt = 0;
+int cnt = 0;
+vector<pair<int, int>> vec;
+
+bool overlapped(int x1, int x2, int y1, int y2)
+{
+    if (x1 == y1 || x1 == y2 || x2 == y1 || x2 == y2) return false;
+    if ((x1 > y1 && y2 > x1) || (y1 > x1 && x2 > y1)) return false;
+    return true;
+}
+
+
+
+bool possible()
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        int x1 = vec[i].first;
+        int x2 = vec[i].second;
+
+        for (int j = 1; j < vec.size(); j++)
+        {
+            int y1 = vec[j].first;
+            int y2 = vec[j].second;
+            if (overlapped(x1, x2, y1, y2)) return false;
+        }
+    }
+
+    return true;
+}
+
+void select(int cur)
+{
+    if (cur == n)
+    {
+        if (possible())
+        {
+            if (best_cnt < vec.size())
+            {
+                best_cnt = vec.size();
+            }
+        }
+        return;
+    }
+
+    vec.push_back(line[cur]);
+    select(cur + 1);
+    vec.pop_back();
+    select(cur + 1);
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        line[i] = make_pair(x, y);
+    }
+
+    select(0);
+
+    cout << best_cnt;
+
+    return 0;
+}
