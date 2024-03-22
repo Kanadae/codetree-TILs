9e457a2b3c385c43b11e5e8f4df717a5879e7ed2
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-22 19:49:33 +0900

    Title: 십자 모양 폭발, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/십자 모양 폭발/cross-shape-bomb.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#define MAX_N 200
+using namespace std;
+
+int n, r, c;
+int grid[MAX_N][MAX_N];
+int temp[MAX_N][MAX_N];
+int end_of_array;
+
+void Gravity(){
+    for(int col=0; col<n; col++){
+        int idx = n-1;
+        for(int row=n-1; row>=0; row--){
+            if(grid[row][col]!=0) temp[idx--][col] = grid[row][col];
+        }
+    }
+}   
+
+void Bomb(int r, int c){
+    int power = grid[r][c];
+    grid[r][c] = 0;
+    for(int i=1; i<power; i++){
+        grid[r-i][c] = 0, grid[r+i][c] = 0;
+        grid[r][c-i] = 0, grid[r][c+i] = 0;
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n; 
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> grid[i][j];
+        }
+    }
+    end_of_array = n;
+    cin >> r >> c;
+    Bomb(r-1, c-1);
+    Gravity();
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << temp[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
