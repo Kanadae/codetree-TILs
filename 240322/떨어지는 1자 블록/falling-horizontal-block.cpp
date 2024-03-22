eb2b126f18a15d8d1ca58a5ced822de755f6d338
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-22 22:23:42 +0900

    Title: 떨어지는 1자 블록, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#define MAX_N 100
+using namespace std;
+
+int n, m, k;
+int grid[MAX_N][MAX_N];
+
+void Fall(int m, int col){
+    for(int i=0; i<n; i++){
+        for(int j=col; j<col+m; j++){
+            if(grid[i][j] == 1){
+                for(int k=col; k<col+m; k++){
+                    grid[i-1][k] = 1;
+                }
+                return;
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> m >> k;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> grid[i][j];
+        }
+    }
+    Fall(m, k-1);
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << grid[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
