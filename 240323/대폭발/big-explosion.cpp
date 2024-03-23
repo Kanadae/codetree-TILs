e661aa02bb6e6ef8c790c61f2a24ae7faf7d6c69
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-23 19:12:11 +0900

    Title: 대폭발, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240323/대폭발/big-explosion.cpp
+++ 240323/대폭발/big-explosion.cpp
@@ -12,18 +12,18 @@ bool InRange(int x, int y){
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void Bomb(int x, int y){
+void Bomb(int x, int y, int dist){
     int dx[DIR_NUM] = {-1, 0, 1, 0}, dy[DIR_NUM] = {0, 1, 0, -1};
     for(int i=0; i<DIR_NUM; i++){
-        int new_x = x + dx[i] * m;
-        int new_y = y + dy[i] * m;
+        int new_x = x + dx[i] * dist;
+        int new_y = y + dy[i] * dist;
         if(InRange(new_x, new_y)){
             copy_grid[new_x][new_y] = 1;
         }
     }
 }
 
-void Simulate(){
+void Simulate(int time){
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             copy_grid[i][j] = grid[i][j];
@@ -32,7 +32,7 @@ void Simulate(){
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(grid[i][j]) Bomb(i, j);
+            if(grid[i][j]) Bomb(i, j, time);
         }
     }
 
@@ -48,8 +48,8 @@ int main() {
     cin.tie(0); cout.tie(0);
     cin >> n >> m >> r >> c;
     grid[r-1][c-1] = 1;
-    for(int t=1; t<=1; t++){
-        Simulate();
+    for(int t=1; t<=m; t++){
+        Simulate(t);
     }
 
     int ans = 0;
