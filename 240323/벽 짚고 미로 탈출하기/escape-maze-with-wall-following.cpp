f88a626d63ce03b9ea9d191fa436b44711ff88fe
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-23 21:08:07 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240323/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
+++ 240323/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int N, row, col, dir;
 char grid[MAX_N][MAX_N];
 int ans;
+bool visited[MAX_N][MAX_N];
 
 bool InRange(int x, int y){
     return 0 <= x && x < N && 0 <= y && y < N;
@@ -14,7 +15,7 @@ bool InRange(int x, int y){
 
 bool Move(int x, int y, int cur_dir){
     int dx[DIR_NUM] = {0, 1, 0, -1}, dy[DIR_NUM] = {1, 0, -1, 0};
-    
+    visited[x][y] = true;
     int new_x = x + dx[cur_dir];
     int new_y = y + dy[cur_dir];
 
@@ -24,6 +25,10 @@ bool Move(int x, int y, int cur_dir){
     }
     ans++;
     if(!InRange(new_x, new_y)) return false;
+    if(visited[new_x][new_y]){
+        ans = -1;
+        return false;
+    }
 
     else if(grid[new_x][new_y] == '.'){
         if(dir == 0 && grid[new_x+1][new_y] != '#') dir = (dir + 1) % 4;
