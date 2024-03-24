80709bc9d8ebd27eb498b7353b6f7d7d3f80d9bf
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-24 23:55:30 +0900

    Title: 마을 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240324/마을 구분하기/seperate-village.cpp
+++ 240324/마을 구분하기/seperate-village.cpp
@@ -1,72 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#define MAX_N 26
+#define DIR_NUM 4
 using namespace std;
-int n;
-int map[26][26];
-bool visited[26][26];
-int city;
-vector<int> people;
-int person;
 
-bool Cango(int x, int y)
-{
-    if (0 > x || x >= n || 0 > y || y >= n) return false;
-    if (visited[x][y]) return false;
-    if (!map[x][y]) return false;
-    return true;
+int n;
+int grid[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
+vector<int> arr;
+int cnt;
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void dfs(int x, int y)
-{
-    if (!visited[x][y]) {
-        visited[x][y] = true;
-        person++;
-    }
-    int dx[4] = { 0,1,0,-1 };
-    int dy[4] = { 1,0,-1,0 };
-    for (int i = 0; i < 4; i++)
-    {
+void DFS(int x, int y){
+    visited[x][y] = true;
+    int dx[DIR_NUM] = {-1, 0, 1, 0}, dy[DIR_NUM] = {0, 1, 0, -1};
+
+    for(int i=0; i<DIR_NUM; i++){
         int new_x = x + dx[i];
         int new_y = y + dy[i];
-        if (Cango(new_x, new_y))
-        {
-            person++;
-            visited[new_x][new_y] = true;
-            dfs(new_x, new_y);
+        if(InRange(new_x, new_y) && !visited[new_x][new_y] && grid[new_x][new_y]){
+            cnt++;
+            DFS(new_x, new_y);
         }
     }
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> map[i][j];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> grid[i][j];
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (map[i][j] == 0) continue;
-            if (!visited[i][j])
-            {
-                dfs(i, j);
-                city++;
-                people.push_back(person);
-                person = 0;
-            }
+    int ans = 0, idx = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(visited[i][j] || !grid[i][j]) continue;
+            visited[i][j] = true;
+            cnt = 1;
+            DFS(i, j);
+            arr.push_back(cnt);
+            ans++;
         }
     }
-    cout << city << "\n";
-    sort(people.begin(), people.end());
-    for (int i = 0; i < city; i++)
-    {
-        cout << people[i] << "\n";
+    cout << ans << "\n";
+    sort(arr.begin(), arr.end());
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << "\n";
     }
     return 0;
 }
