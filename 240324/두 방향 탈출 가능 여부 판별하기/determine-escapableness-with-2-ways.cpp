161064f5fec77b3ae2bd5ca93e2b3f2dfb2c425e
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-24 23:35:14 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1096ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240324/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240324/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -12,14 +12,13 @@ bool InRange(int x, int y){
 }
 
 void DFS(int x, int y){
-    int dx[2] = {1, 0}, dy[2] = {0, 1};
+    int dx[2] = {0, 1}, dy[2] = {1, 0};
 
     for(int i=0; i<2; i++){
         int new_x = x + dx[i];
         int new_y = y + dy[i];
         if(InRange(new_x, new_y) && grid[new_x][new_y]){
             visited[new_x][new_y] = true;
-            if(visited[n-1][m-1]) return;
             DFS(new_x, new_y);
         }
     }
