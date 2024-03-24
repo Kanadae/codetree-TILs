7dc4a3d715a0f956927247aa926f2f9815ec41b7
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-24 21:59:33 +0900

    Title: 그래프 탐색, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240324/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,m;
+vector<int> graph[1000];
+int visited[1000];
+int ans = 0;
+void bfs(int cnt)
+{
+    for (int i = 0; i < graph[cnt].size(); i++)
+    {
+        int cur = graph[cnt][i];
+        if(!visited[cur])
+        {
+            ans++;
+            visited[cur] = true;
+            bfs(cur);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int x,y;
+        cin >> x >> y;
+        graph[x].push_back(y);
+        graph[y].push_back(x);
+    }
+
+    bfs(1);
+    cout << --ans;
+    return 0;
+}
