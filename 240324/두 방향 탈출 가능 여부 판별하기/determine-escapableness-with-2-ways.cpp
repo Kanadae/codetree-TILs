1e30614755f81b9552fb7a08cf38737da0038a96
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-24 23:29:04 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240324/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240324/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -1,47 +1,41 @@
 #include <iostream>
-#include <vector>
+
+#define MAX 101
 using namespace std;
 
-int n,m;
-int visited[101][101];
-int map[101][101];
-int ans;
+int n, m;
+int grid[MAX][MAX];
+bool visited[MAX][MAX];
 
-bool Cango(int x, int y)
-{
-    if (0 > x || x >= n || 0 > y || y >= m) return false;
-    if (!map[x][y]) return false;
-    if (visited[x][y]) return false;
-    return true;
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < m;
 }
 
-void dfs(int x, int y)
-{
-    int dx[2] = {0, 1};
-    int dy[2] = {1, 0};
-    visited[x][y] = 1;
-    for (int i = 0; i < 2; i++)
-    {
+void DFS(int x, int y){
+    int dx[2] = {1, 0}, dy[2] = {0, 1};
+
+    for(int i=0; i<2; i++){
         int new_x = x + dx[i];
         int new_y = y + dy[i];
-        if (Cango(new_x,new_y))
-        {
-            dfs(new_x,new_y);
+        if(InRange(new_x, new_y) && !grid[new_x][new_y]){
+            visited[new_x][new_y] = true;
+            DFS(new_x, new_y);
         }
-    }    
+    }
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> map[i][j];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> grid[i][j];
         }
-    }    
-
-    dfs(0,0);
-    if (visited[n-1][m-1]) cout << 1; else cout << 0;
+    }
+    visited[0][0] = true;
+    DFS(0, 0);
+    if(visited[n-1][m-1]) cout << "1";
+    else cout << "0";
     return 0;
 }
