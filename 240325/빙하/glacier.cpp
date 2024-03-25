e0ceacaebb81ae8acc840fc33b9e183d340b6527
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-25 19:28:05 +0900

    Title: 빙하, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240325/빙하/glacier.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+queue<pair<int,int>> q;
+int n, m, t;
+int map[201][201];
+int visited[201][201];
+int heat;
+
+bool canGo(int x, int y)
+{
+    if (0 > x || x >= n || 0 > y || y >= m) return false;
+    if (visited[x][y]) return false;
+    return true;
+}
+
+void bfs()
+{
+    int dx[4] = {0,1,0,-1};
+    int dy[4] = {1,0,-1,0};
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int new_x = x + dx[i];
+            int new_y = y + dy[i];
+            if (canGo(new_x,new_y))
+            {
+                if (map[new_x][new_y] == 1)
+                {
+                    visited[new_x][new_y] = true;
+                    map[new_x][new_y] = 0;
+                    heat++;
+                } else {
+                    visited[new_x][new_y] = true;
+                    q.push(make_pair(new_x,new_y));
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> map[i][j];
+        }
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (map[i][j] == 0 && !visited[i][j])
+            {
+                heat = 0;
+                q.push(make_pair(i,j));
+                bfs();
+                t++;
+            }
+        }
+    }
+    cout << t << " " << heat;
+    return 0;
+}
