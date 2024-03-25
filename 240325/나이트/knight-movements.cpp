8a1cde054f0c2e599c34f4d61a0cd04b3c8f277e
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-25 19:50:12 +0900

    Title: 나이트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240325/나이트/knight-movements.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+
+#define DIR_NUM 8
+#define MAX_N 101
+using namespace std;
+
+int n;
+int r1, r2, c1, c2;
+int grid[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
+int step[MAX_N][MAX_N];
+
+queue<pair<int, int>> q;
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+void bfs(){
+    int dx[DIR_NUM] = {-2, -2, -1, -1, 1, 1, 2, 2};
+    int dy[DIR_NUM] = {-1, 1, -2, 2, -2, 2, 1, -1};
+
+    while(!q.empty()){
+        pair<int, int> cur_pos = q.front();
+        int x = cur_pos.first;
+        int y = cur_pos.second;
+        q.pop();
+
+        for(int i=0; i<DIR_NUM; i++){
+            int new_x = x + dx[i];
+            int new_y = y + dy[i];
+            if(InRange(new_x, new_y) && !visited[new_x][new_y]){
+                visited[new_x][new_y] = true;
+                step[new_x][new_y] = step[x][y] + 1;
+                if(new_x == r2-1 && new_y == c2-1) return;
+                q.push(make_pair(new_x, new_y));
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
+    cin >> r1 >> c1 >> r2 >> c2;
+    visited[r1-1][c1-1] = true;
+    q.push(make_pair(r1-1, c1-1));
+    step[r2-1][c2-1] = -1;
+    bfs();
+    cout << step[r2-1][c2-1];
+    return 0;
+}
