a8db4f2e7984d4bc8b69d4188f866e6e472ff80a
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-25 17:06:47 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240325/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 240325/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -1,53 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#define MAX_N 100
 using namespace std;
 int n, m;
-int map[101][101];
-bool visited[101][101];
-queue<pair<int,int>> q;
+int grid[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
 
-bool Cango(int x, int y)
-{
-    if (0 > x || x >= n || 0 > y || y >= m) return false;
-    if (visited[x][y]) return false;
-    if (!map[x][y]) return false;
-    return true;
+queue<pair<int, int>> q;
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void bfs()
-{
-    int dx[4] = {0,1,0,-1};
-    int dy[4] = {1,0,-1,0};
-    while(!q.empty())
-    {
-        int x = q.front().first;
-        int y = q.front().second;
+void BFS(){
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
+
+    while(!q.empty()){
+        pair<int, int> cur_pos = q.front();
         q.pop();
-        for (int i = 0; i < 4; i++)
-        {
+
+        int x = cur_pos.first;
+        int y = cur_pos.second;
+        for(int i=0; i<4; i++){
             int new_x = x + dx[i];
             int new_y = y + dy[i];
-            if (Cango(new_x,new_y))
-            {
+            if(InRange(new_x, new_y) && !visited[new_x][new_y] && grid[new_x][new_y]){
                 visited[new_x][new_y] = true;
-                q.push(make_pair(new_x,new_y));
+                q.push(make_pair(new_x, new_y));
             }
-       }    
-    }
-}
-
-int main() {
-    cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> map[i][j];
         }
     }
-    q.push(make_pair(0,0));
-    bfs();
-    if (visited[n-1][m-1]) cout << 1; else cout << 0;
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> grid[i][j];
+        }
+    }
+    q.push(make_pair(0, 0));
+    BFS();
+    if(visited[n-1][m-1]) cout << "1";
+    else cout << "0";
     return 0;
 }
