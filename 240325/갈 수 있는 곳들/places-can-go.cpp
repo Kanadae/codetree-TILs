4f670895d35c71ce7c53d44d52620c1ac62045ca
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-25 17:22:12 +0900

    Title: 갈 수 있는 곳들, Time: 1055ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240325/갈 수 있는 곳들/places-can-go.cpp
+++ 240325/갈 수 있는 곳들/places-can-go.cpp
@@ -1,66 +1,60 @@
 #include <iostream>
 #include <queue>
-using namespace std;
-int n, k;
-queue<pair<int,int>> q;
-bool visited[101][101];
-int map[101][101];
-int ans;
 
-bool Cango(int x, int y)
-{
-    if (0 > x || x >= n || 0 > y || y >= n) return false;
-    if (visited[x][y]) return false;
-    if (map[x][y]) return false;
-    return true;
+#define MAX_N 101
+#define DIR_NUM 4
+using namespace std;
+
+int n, k;
+int grid[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
+queue<pair<int, int>> q;
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void bfs()
-{
-    int dx[4] = {0, 1, 0, -1};
-    int dy[4] = {1, 0, -1, 0};
-    while (!q.empty())
-    {
-        int x, y;
-        x = q.front().first;
-        y = q.front().second;
-        q.pop();
-        for (int i = 0; i < 4; i++)
-        {
+void BFS(){
+    int dx[DIR_NUM] = {-1, 0, 1, 0}, dy[DIR_NUM] = {0, 1, 0, -1};
+
+    while(!q.empty()){
+        pair<int, int> cur_pos = q.front();
+        int x = cur_pos.first;
+        int y = cur_pos.second;
+
+        for(int i=0; i<DIR_NUM; i++){
             int new_x = x + dx[i];
             int new_y = y + dy[i];
-            if (Cango(new_x,new_y))
-            {
+            if(InRange(new_x, new_y) && !visited[new_x][new_y] && !grid[new_x][new_y]){
                 visited[new_x][new_y] = true;
-                ans++;
-                q.push(make_pair(new_x,new_y));
+                q.push(make_pair(new_x, new_y));
             }
         }
     }
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     cin >> n >> k;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> map[i][j];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> grid[i][j];
         }
     }
 
-    for (int i = 0; i < k; i++)
-    {
-        int x,y;
-        cin >> x >> y;
-        x--; y--;
-        if (!visited[x][y])
-        {
-            ans++;
-            visited[x][y] = true;
+    while(k--){
+        int r, c;
+        cin >> r >> c;
+        visited[r-1][c-1] = true;
+        q.push(make_pair(r-1, c-1));
+        BFS();
+    }
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(visited[i][j]) ans++;
         }
-        q.push(make_pair(x,y));
-        bfs();
     }
     cout << ans;
     return 0;
