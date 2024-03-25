cce0eb6ff50ffdc6e571ba7d29b4923c85d370e4
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-25 17:56:20 +0900

    Title: K번 최댓값으로 이동하기, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240325/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
+++ 240325/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
@@ -1,94 +1,105 @@
 #include <iostream>
 #include <queue>
-using namespace std;
-int n,k;
-int start_x , start_y;
-int map[101][101];
-bool visited[101][101];
-queue<pair<int, int>> q;
-int maxi;
-pair<int,int> maxi_locate;
-int cur;
+#include <tuple>
 
-bool Cango(int x, int y)
-{
-    if (0 > x || x >= n || 0 > y || y >= n) return false;
-    if (visited[x][y]) return false;
-    if (map[x][y] >= cur) return false;
-    return true;
+#define MAX_N 100
+#define DIR_NUM 4
+using namespace std;
+
+int n, k;
+int target;
+int grid[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
+queue<pair<int, int>> q;
+pair<int, int> cur_num;
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void bfs()
-{
-    int dx[4] = {0, 1 ,0, -1};
-    int dy[4] = {1, 0, -1, 0};
-    while (!q.empty())
-    {
-        int x,y;
-        x = q.front().first;
-        y = q.front().second;
+bool NeedUpdate(pair<int, int> best_pos, pair<int, int> new_pos) {
+    if(best_pos == make_pair(-1, -1))
+        return true;
+    
+    int best_x, best_y;
+    tie(best_x, best_y) = best_pos;
+    
+    int new_x, new_y;
+    tie(new_x, new_y) = new_pos;
+    
+    return make_tuple(grid[new_x][new_y], -new_x, -new_y) >
+           make_tuple(grid[best_x][best_y], -best_x, -best_y);
+}
+
+void BFS(){
+    int dx[DIR_NUM] = {-1, 0, 1, 0}, dy[DIR_NUM] = {0, 1, 0, -1};
+
+    int cur_x, cur_y;
+    tie(cur_x, cur_y) = cur_num;
+    visited[cur_x][cur_y] = true;
+    q.push(cur_num);
+    target = grid[cur_x][cur_y];
+    
+    while(!q.empty()){
+        pair<int, int> cur_pos = q.front();
+        int x = cur_pos.first;
+        int y = cur_pos.second;
         q.pop();
-        for (int i = 0; i < 4; i++)
-        {
+
+        for(int i=0; i<DIR_NUM; i++){
             int new_x = x + dx[i];
             int new_y = y + dy[i];
-            if (Cango(new_x,new_y))
-            {
-                if (maxi < map[new_x][new_y]) {
-                    maxi = map[new_x][new_y];
-                    maxi_locate = make_pair(new_x,new_y);
-                } else if (maxi == map[new_x][new_y])
-                {
-                    int mx = maxi_locate.first;
-                    int my = maxi_locate.second;
-                    if (mx > new_x)
-                    {
-                        maxi_locate = make_pair(new_x,new_y);
-                    } else if (mx == new_x)
-                    {
-                        if (my > new_y) maxi_locate = make_pair(new_x,new_y);
-                    }
-                }
+            if(InRange(new_x, new_y) && !visited[new_x][new_y] && grid[new_x][new_y] < target){
                 visited[new_x][new_y] = true;
-                q.push(make_pair(new_x,new_y));
+                q.push(make_pair(new_x, new_y));
             }
         }
     }
 }
 
-int main() {
-    cin >> n >> k;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> map[i][j];
+bool Simulate(){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            visited[i][j] = false;
         }
-    }
-    cin >> start_x >> start_y;
-    start_x--; start_y--;
-    int cnt = 0;
-    while (cnt < k)
-    {
-        cur = map[start_x][start_y];
-        q.push(make_pair(start_x,start_y));
-        bfs();
-        if (maxi == 0) break;
-        start_x = maxi_locate.first;
-        start_y = maxi_locate.second;
-        cur = maxi;
-       
-        maxi = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                visited[i][j] = false;
-            }
-        }
-        cnt++;
     }
 
-    cout << ++start_x << " " << ++start_y;
+    BFS();
+    pair<int, int> ans_pos = make_pair(-1, -1);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(!visited[i][j] || cur_num == make_pair(i, j)) continue;
+
+            pair<int, int> new_pos = make_pair(i, j);
+            if(NeedUpdate(ans_pos, new_pos))
+                ans_pos = new_pos;
+        }
+    }
+
+    if(ans_pos == make_pair(-1, -1)) return false;
+    else {
+        cur_num = ans_pos;
+        return true;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> k;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> grid[i][j];
+        }
+    }
+    int r, c;
+    cin >> r >> c;
+    cur_num = make_pair(r-1, c-1);
+    while(k--){
+        if(!Simulate()) break;
+    }
+
+    int ans_x, ans_y;
+    tie(ans_x, ans_y) = cur_num;
+    cout << ans_x+1 << " " << ans_y+1;
     return 0;
 }
