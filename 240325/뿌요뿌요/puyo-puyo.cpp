fd6bdda5a091b63213bb0b14882abb356bbfb621
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-25 15:54:23 +0900

    Title: 뿌요뿌요, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240325/뿌요뿌요/puyo-puyo.cpp
+++ 240325/뿌요뿌요/puyo-puyo.cpp
@@ -1,59 +1,55 @@
 #include <iostream>
-using namespace std;
-int n;
-int map[101][101];
-int visited[101][101];
-int max_block;
-int block;
-int pop_block;
 
-bool Cango(int x, int y)
-{
-    if (0 > x || x >= n || 0 > y || y >= n) return false;
-    if (visited[x][y]) return false;
-    return true;
+#define MAX_N 100
+#define DIR_NUM 4
+using namespace std;
+
+int n;
+int grid[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
+int ans;
+int area;
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void dfs(int x, int y, int num)
-{
-    int dx[4] = {0,1,0,-1};
-    int dy[4] = {1,0,-1,0};
-    for (int i = 0; i < 4; i++)
-    {
+void DFS(int x, int y, int target){
+    int dx[DIR_NUM] = {-1, 0, 1, 0}, dy[DIR_NUM] = {0, 1, 0, -1};
+    visited[x][y] = true;
+    area++;
+    for(int i=0; i<DIR_NUM; i++){
         int new_x = x + dx[i];
         int new_y = y + dy[i];
-        if (Cango(new_x,new_y) && map[new_x][new_y] == num)
-        {
-            block++;
-            visited[new_x][new_y] = true;
-            dfs(new_x,new_y,num);
+        if(InRange(new_x, new_y) && !visited[new_x][new_y] && grid[new_x][new_y] == target){
+            DFS(new_x, new_y, target);
         }
     }
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> map[i][j];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> grid[i][j];
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (visited[i][j]) continue;
-            block++;
-            visited[i][j] = true;
-            dfs(i,j,map[i][j]);
-            if (block >= 4) pop_block++;
-            if (max_block < block) max_block = block;
-            block = 0;               
-        }
-    }
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(visited[i][j]) continue;
 
-    cout << pop_block << " " << max_block;
+            int target = grid[i][j];
+            DFS(i, j, target);
+            if(area > ans) {
+                ans = area;
+                cnt++;
+            }
+            area = 0;
+        }
+    }
+    cout << cnt << " " << ans;
     return 0;
 }
