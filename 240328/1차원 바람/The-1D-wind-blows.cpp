06794718bf3ea49a734db9ca19e0d724330bb009
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-28 13:11:28 +0900

    Title: 1차원 바람, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240328/1차원 바람/The-1D-wind-blows.cpp
+++ 240328/1차원 바람/The-1D-wind-blows.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int N, M, Q;
 int grid[MAX][MAX];
-bool visited[MAX];
 
 bool InRange(int x){
     return 0 <= x && x < N;
@@ -16,11 +15,8 @@ int Dir(char dir){
     else return 1;
 }
 
-void Simulate(int row, int dir){
+void Shift(int row, int dir){
     int temp;
-    visited[row] = true;
-    bool up = false;
-    bool down = false;
 
     if(dir == 0) {
         temp = grid[row][M-1];
@@ -36,18 +32,39 @@ void Simulate(int row, int dir){
         }
         grid[row][M-1] = temp;
     }
+}
 
+bool Compare(int row1, int row2){
     for(int i=0; i<M; i++){
-        if(grid[row][i] == grid[row-1][i]) up = true;
-        if(grid[row][i] == grid[row+1][i]) down = true;
+        if(grid[row1][i] == grid[row2][i]) return true;
     }
-    for(int i=0; i<M; i++){
-        if(InRange(row-1) && !visited[row-1] && up){
-            Simulate(row-1, ~dir);
+    return false;
+}
+
+void Simulate(int row, int dir){
+    Shift(row, dir);
+    int up_dir = !dir;
+    int down_dir = !dir;
+
+    int up_row = row;
+    int down_row = row;
+
+    while(InRange(up_row-1)){
+        if(Compare(up_row, up_row-1)){
+            Shift(up_row-1, up_dir);
+            up_dir = !up_dir;
+            up_row--;
         }
-        else if(InRange(row+1) && !visited[row+1] && down){
-            Simulate(row+1, ~dir);
+        else break;
+    }
+
+    while(InRange(down_row+1)){
+        if(Compare(down_row, down_row+1)){
+            Shift(down_row+1, down_dir);
+            down_dir = !down_dir;
+            down_row++;
         }
+        else break;
     }
 }
 
@@ -62,10 +79,8 @@ int main() {
     }
     int row;
     char dir;
-
     while(Q--){
         cin >> row >> dir;
-        for(int i=0; i<N; i++) visited[i] = false;
         Simulate(row-1, Dir(dir));
         
     }
