0df13445a1a952a753a0e1241216a30157a5512c
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-03-28 22:58:30 +0900

    Title: 떨어지는 1자 블록, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240328/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int n, length, r;
+int map[101][101];
+
+int main() {
+    cin >> n >> length >> r;
+    r--;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> map[i][j];
+        }
+    }
+    int stop = n - 1;
+    int sig = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = r; j < length; j++)
+        {
+            if (map[i][j] == 1)
+            {
+                stop = i-1;
+                sig = 1;
+                break;
+            }
+        }
+        if (sig) break;
+    }
+
+    int idx = r;
+    for (int i = 0; i < length; i++)
+    {
+        map[stop][idx++] = 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << map[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
