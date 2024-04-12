7bbeadf6c4910f5454335b9482e69e05f9d42754
Jihyeon Park <142780681+Kanadae@users.noreply.github.com>
2024-04-12 16:59:39 +0900

    Title: 정육면체 한번 더 굴리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240412/정육면체 한번 더 굴리기/cube-rounding-again.cpp
+++ 240412/정육면체 한번 더 굴리기/cube-rounding-again.cpp
@@ -1,117 +1,120 @@
 #include <iostream>
-#include <tuple>
 #include <queue>
-
-#define MAX_N 20
-#define DIR_NUM 4
 using namespace std;
-
 int n, m;
-int grid[MAX_N][MAX_N];
-int up = 1, front = 2, ryght = 3;
-int dir = 1;
-pair<int, int> dice;
-int ans;
-bool visited[MAX_N][MAX_N];
-queue<pair<int, int>> q;
+int map[21][21];
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0}; // 우,하,좌,상
+int u = 1, f = 2, r = 3;
+int dir = 0; // 현재 방향
+int curx = 0, cury = 0; // 현재 위치
+int d; // 아랫면
+bool visited[21][21];
+queue<pair<int,int>> q;
+int temp = 0;
+int ans = 0;
 
-bool InRange(int x, int y) {
+bool inRange(int x,int y)
+{
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-void Init() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            visited[i][j] = false;
-        }
-    }
-}
-
-void BFS(int target) {
-    int dx[DIR_NUM] = { -1, 0, 1, 0 }, dy[DIR_NUM] = { 0, 1, 0, -1 };
-
-    while (!q.empty()) {
-        pair<int, int> cur_pos = q.front();
-        int x = cur_pos.first;
-        int y = cur_pos.second;
+void bfs()
+{
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
         q.pop();
-        for (int i = 0; i < DIR_NUM; i++) {
-            int new_x = x + dx[i];
-            int new_y = y + dy[i];
-            if (InRange(new_x, new_y) && !visited[new_x][new_y] && grid[new_x][new_y] == target) {
-                ans += target;
-                visited[new_x][new_y] = true;
-                q.push(make_pair(new_x, new_y));
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (!inRange(nx,ny) || visited[nx][ny]) continue;
+            if (map[nx][ny] == map[curx][cury])
+            {
+                visited[nx][ny] = true;
+                q.push(make_pair(nx,ny));
+                temp++;
             }
         }
     }
 }
 
-void Simulate() {
-    int dx[DIR_NUM] = { -1, 0, 1, 0 }, dy[DIR_NUM] = { 0, 1, 0, -1 };
-    int x, y;
-    tie(x, y) = dice;
-
-    int new_x = x + dx[dir];
-    int new_y = y + dy[dir];
-    if (!InRange(new_x, new_y)) {
-        dir = (dir + 2) % 4;
-        new_x = x + dx[dir];
-        new_y = y + dy[dir];
+void simulate()
+{
+    // 방향대로 한칸 이동
+    curx = curx + dx[dir];
+    cury = cury + dy[dir];
+    // 주사위 굴리기
+    if (dir == 0) // 우
+    {
+        u = 7-r;
+        f = f;
+        r = u;
+    } else if (dir == 1)
+    {
+        u = 7 - f;
+        f = u;
+        r = r;
+    } else if (dir == 2)
+    {
+        u = r;
+        f = f;
+        r = 7 - u;
+    } else if (dir == 3)
+    {
+        u = f;
+        f = 7-u;
+        r = r;
     }
 
-    if (dir == 0) {
-        int temp = up;
-        up = front;
-        front = 7 - temp;
-    }
-    else if (dir == 1) {
-        int temp = ryght;
-        ryght = up;
-        up = 7 - temp;
-    }
-    else if (dir == 2) {
-        int temp = front;
-        front = up;
-        up = 7 - temp;
-    }
-    else if (dir == 3) {
-        int temp = up;
-        up = ryght;
-        ryght = 7 - temp;
-    }
+    d = 7 - u;
 
-    // cout << up << " " << front << " " << ryght << "\n";
-
-    dice = make_pair(new_x, new_y);
-    Init();
-    q.push(make_pair(new_x, new_y));
-    visited[new_x][new_y] = true;
-    ans += grid[new_x][new_y];
-    BFS(grid[new_x][new_y]);
-
-    if (7 - up > grid[new_x][new_y]) {
+    visited[curx][cury] = true;
+    temp = 1;
+    q.push(make_pair(curx,cury));
+    bfs();
+    ans += temp * map[curx][cury];
+    if (d > map[curx][cury])
+    {
         dir = (dir + 1) % 4;
-    }
-    else if (7 - up < grid[new_x][new_y]) {
+    } else if (d < map[curx][cury])
+    {
         dir = (dir + 3) % 4;
     }
+    int nx = curx + dx[dir];
+    int ny = cury + dy[dir];
+    if (!inRange(nx,ny))
+    {
+        if (dir < 2) dir += 2;
+        else dir -= 2;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            visited[i][j] = false;
+        }
+    }
+    
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
     cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> grid[i][j];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> map[i][j];
         }
     }
-
-    while (m--) {
-        Simulate();
-        // cout << ans << "\n";
+    for (int i = 0; i < m; i++)
+    {
+        simulate();
     }
+
     cout << ans;
     return 0;
 }
